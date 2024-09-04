47c772180def26bd8855de94918b25a404756bff
Toma Rosa <toma.rosa@ik.me>
2024-09-04 17:04:09 +0200

    feat: dialog

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -1,16 +1,19 @@
 import { Dialog, Portal } from "@ark-ui/react";
+import { XIcon } from "lucide-react";
 
 export function DialogExample() {
   return (
     <Dialog.Root>
-      <Dialog.Trigger>Open Dialog</Dialog.Trigger>
+      <Dialog.Trigger>Open</Dialog.Trigger>
       <Portal>
         <Dialog.Backdrop />
         <Dialog.Positioner>
           <Dialog.Content>
             <Dialog.Title>Dialog Title</Dialog.Title>
             <Dialog.Description>Dialog Description</Dialog.Description>
-            <Dialog.CloseTrigger>Close</Dialog.CloseTrigger>
+            <Dialog.CloseTrigger>
+              <XIcon />
+            </Dialog.CloseTrigger>
           </Dialog.Content>
         </Dialog.Positioner>
       </Portal>

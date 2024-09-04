c4050a7a80ee3faebc76733200685f88c9d08577
Toma Rosa <toma.rosa@ik.me>
2024-09-04 13:23:51 +0200

    chore(avatar): css simplification

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { Accordion } from "@ark-ui/react";
-import { ChevronDownIcon } from "lucide-react";
+import { AvatarExample } from "./components/avatar/avatar";
 
 function App() {
   return (
     <div className="min-h-dvh grid place-items-center">
-      <Accordion.Root collapsible className="w-full">
-        {["React", "Solid", "Vue"].map((item) => (
-          <Accordion.Item key={item} value={item}>
-            <Accordion.ItemTrigger>
-              What is {item}?
-              <Accordion.ItemIndicator>
-                <ChevronDownIcon />
-              </Accordion.ItemIndicator>
-            </Accordion.ItemTrigger>
-            <Accordion.ItemContent>
-              <div className="pb-3 pt-0">
-                {item} is a JavaScript library for building user interfaces.
-              </div>
-            </Accordion.ItemContent>
-          </Accordion.Item>
-        ))}
-      </Accordion.Root>
+      <AvatarExample />
     </div>
   );
 }

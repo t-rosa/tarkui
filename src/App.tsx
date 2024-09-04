56a1814a8650f9ba65e3102788a98734b2334134
Toma Rosa <toma.rosa@ik.me>
2024-09-04 13:28:35 +0200

    fix(checkbox): add missing styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { AvatarExample } from "./components/avatar/avatar";
+import { CheckboxExample } from "./components/checkbox/checkbox";
 
 function App() {
   return (
     <div className="min-h-dvh grid place-items-center">
-      <AvatarExample />
+      <CheckboxExample />
     </div>
   );
 }

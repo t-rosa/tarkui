7712aaae902eecc8cfada7f06bff44a4cd02fe3c
Toma Rosa <toma.rosa@ik.me>
2024-09-05 09:15:50 +0200

    feat: accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import { FieldExample } from "./components/field/field";
+import { ChevronDownIcon } from "lucide-react";
+import { Accordion } from "./components/accordion/accordion";
 
 function App() {
   return (
     <div className="min-h-dvh grid place-items-center">
-      <FieldExample />
+      <Accordion.Root defaultValue={["React"]}>
+        {["React", "Solid", "Vue"].map((item) => (
+          <Accordion.Item key={item} value={item}>
+            <Accordion.ItemTrigger>
+              What is {item}?
+              <Accordion.ItemIndicator>
+                <ChevronDownIcon />
+              </Accordion.ItemIndicator>
+            </Accordion.ItemTrigger>
+            <Accordion.ItemContent>
+              {item} is a JavaScript library for building user interfaces.
+            </Accordion.ItemContent>
+          </Accordion.Item>
+        ))}
+      </Accordion.Root>
     </div>
   );
 }
--- src/components/accordion/accordion.tsx
+++ src/components/accordion/accordion.tsx
@@ -1,24 +1,89 @@
-import { Accordion } from "@ark-ui/react";
-import { ChevronDownIcon } from "lucide-react";
+import * as React from "react";
+import { Accordion as ArkAccordion } from "@ark-ui/react";
+import clsx from "clsx";
 
-export function AccordionExample() {
-  return (
-    <Accordion.Root collapsible>
-      {["React", "Solid", "Vue"].map((item) => (
-        <Accordion.Item key={item} value={item}>
-          <Accordion.ItemTrigger>
-            What is {item}?
-            <Accordion.ItemIndicator>
-              <ChevronDownIcon />
-            </Accordion.ItemIndicator>
-          </Accordion.ItemTrigger>
-          <Accordion.ItemContent>
-            <div className="pb-3 pt-0">
-              {item} is a JavaScript library for building user interfaces.
-            </div>
-          </Accordion.ItemContent>
-        </Accordion.Item>
-      ))}
-    </Accordion.Root>
-  );
+export function Accordion(props: React.PropsWithChildren) {
+  return props.children;
 }
+
+const Root = React.forwardRef<HTMLDivElement, ArkAccordion.RootProps>(
+  (props, ref) => (
+    <ArkAccordion.Root
+      ref={ref}
+      className={clsx([props.className])}
+      {...props}
+    />
+  )
+);
+
+Root.displayName = "Root";
+
+const Item = React.forwardRef<HTMLDivElement, ArkAccordion.ItemProps>(
+  (props, ref) => (
+    <ArkAccordion.Item
+      ref={ref}
+      className={clsx(["border-b grid", props.className])}
+      {...props}
+    />
+  )
+);
+
+Item.displayName = "Item";
+
+const ItemContent = React.forwardRef<
+  HTMLDivElement,
+  ArkAccordion.ItemContentProps
+>((props, ref) => (
+  <ArkAccordion.ItemContent
+    ref={ref}
+    className={clsx([
+      "overflow-hidden text-sm",
+      "data-[state=open]:animate-accordion-down",
+      "data-[state=closed]:animate-accordion-up",
+      props.className,
+    ])}
+    {...props}
+  />
+));
+
+ItemContent.displayName = "ItemContent";
+
+const ItemTrigger = React.forwardRef<
+  HTMLButtonElement,
+  ArkAccordion.ItemTriggerProps
+>((props, ref) => (
+  <ArkAccordion.ItemTrigger
+    ref={ref}
+    className={clsx([
+      "flex items-center hover:underline justify-between py-4 text-sm font-medium",
+      props.className,
+    ])}
+    {...props}
+  />
+));
+
+ItemTrigger.displayName = "ItemTrigger";
+
+const ItemIndicator = React.forwardRef<
+  HTMLDivElement,
+  ArkAccordion.ItemIndicatorProps
+>((props, ref) => (
+  <ArkAccordion.ItemIndicator
+    ref={ref}
+    className={clsx([
+      "duration-200 text-muted-foreground",
+      "data-[state=open]:rotate-180",
+      "[&>svg]:size-4",
+      props.className,
+    ])}
+    {...props}
+  />
+));
+
+ItemIndicator.displayName = "ItemIndicator";
+
+Accordion.Root = Root;
+Accordion.Item = Item;
+Accordion.ItemTrigger = ItemTrigger;
+Accordion.ItemContent = ItemContent;
+Accordion.ItemIndicator = ItemIndicator;

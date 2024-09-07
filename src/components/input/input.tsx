20ab3d5ff6b5c7f7c01874d1a240a04d771dc3c0
Toma Rosa <toma.rosa@ik.me>
2024-09-07 10:21:02 +0200

    feat: input

---
+++ src/components/input/input.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+import * as React from "react";
+
+const Input = React.forwardRef<
+  HTMLInputElement,
+  React.InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+  return (
+    <input
+      type={props.type}
+      className={clsx([
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background",
+        "placeholder:text-muted-foreground",
+        "focus-visible:ring-2",
+        "focus-visible:ring-ring",
+        "focus-visible:outline-none",
+        "focus-visible:ring-offset-2",
+        "disabled:cursor-not-allowed ",
+        "disabled:opacity-50",
+        props.className,
+      ])}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Input.displayName = "Input";
+
+export { Input };

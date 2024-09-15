c34738010caf2a9a810e6f22e054729d930afe03
Toma Rosa <toma.rosa@ik.me>
2024-09-15 15:03:45 +0200

    feat: wip menu styles

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,7 +3,7 @@ import { clsx } from "clsx";
 import * as React from "react";
 
 const buttonVariants = cva(
-  "px-3 py-1.5 inline-flex isolate items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "px-3 py-1.5 inline-flex isolate items-center justify-center whitespace-nowrap rounded-md focus-visible: font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
--- src/components/menu/menu-example.tsx
+++ src/components/menu/menu-example.tsx
@@ -1,16 +1,27 @@
+import { Button } from "../button/button";
 import { Menu } from "./menu";
 
 export function MenuExample() {
   return (
     <Menu.Root>
       <Menu.Trigger>
-        Open menu <Menu.Indicator>➡️</Menu.Indicator>
+        <Button>Open</Button>
       </Menu.Trigger>
       <Menu.Positioner>
-        <Menu.Content>
-          <Menu.Item value="react">React</Menu.Item>
-          <Menu.Item value="solid">Solid</Menu.Item>
-          <Menu.Item value="vue">Vue</Menu.Item>
+        <Menu.Content className="z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2">
+          <Menu.Item
+            value="react"
+            className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:cursor-pointer hover:bg-muted"
+          >
+            React
+          </Menu.Item>
+          <Menu.Separator className="-mx-1 my-1 h-px bg-muted" />
+          <Menu.Item
+            value="react"
+            className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 hover:cursor-pointer hover:bg-muted"
+          >
+            React
+          </Menu.Item>
         </Menu.Content>
       </Menu.Positioner>
     </Menu.Root>
